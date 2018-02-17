59b275449fff39b3c8b8a0ba89379d268b9d7b44
wlawler45 <williamgmlawler@yahoo.com>
2018-02-16 20:27:16 -0800

    Add files via upload

--- ADCReader/main.c
+++ ADCReader/main.c
@@ -6,6 +6,7 @@
 #include "Plx.h"
 #include "PlxApi.h"
 #include "PlxInit.h"
+//List of variables associated with ADC chip initialization and PCI configuration registers
 U16 BCR_address=0x0000;
 U32 BCR_register= 0x008310;
 U16 PLL_address= 0x0004;
@@ -22,135 +23,88 @@ U32 buffer_on_register=0x00380FFE; ///to turn on collection
 
 U16 ChipTypeSelected;
 U8  ChipRevision;
-void initDMA(PLX_DEVICE_OBJECT *Device, PLX_DEVICE_KEY *DeviceKey){
-    PLX_STATUS        rc;
-    PLX_DRIVER_PROP   DriverProp;
-    rc =
-        PlxPci_DeviceOpen(
-            &DeviceKey,
-            &Device
-            );
-
-    if (rc != PLX_STATUS_OK)
-    {
-        exit(-1);
-    }
-    PlxPci_DriverProperties(
-                    &Device,
-                    &DriverProp
-                    );
-
-                if (DriverProp.bIsServiceDriver)
-                {
-                    exit(-1);
-                }
-
-
-
-}
-
+/*
+Initialize the PCI chip using the PLX API, enables the interrupt as well
+*/
 PLX_DEVICE_OBJECT pciInit(){
 
-     PLX_STATUS        rc;
+    PLX_STATUS        rc;
     PLX_DEVICE_KEY    DeviceKey;
     PLX_DEVICE_OBJECT Device;
     PLX_INTERRUPT     PlxInterrupt;
+    //printf("*ERROR* - API failed\n");
+    rc =PlxPci_DeviceOpen(&DeviceKey,&Device);
+    printf("\nSelected: %04x %04x [b:%02x  s:%02x  f:%x]\n\n",DeviceKey.DeviceId, DeviceKey.VendorId,DeviceKey.bus, DeviceKey.slot, DeviceKey.function);
 
-      rc =PlxPci_DeviceOpen(
-            &DeviceKey,
-            &Device
-            );
-
-
-
-    /*if (rc != PLX_STATUS_OK)
-    {
-
-        exit(-1);
-    } */
-    printf(
-        "\nSelected: %04x %04x [b:%02x  s:%02x  f:%x]\n\n",
-        DeviceKey.DeviceId, DeviceKey.VendorId,
-        DeviceKey.bus, DeviceKey.slot, DeviceKey.function
-        );
-
-    PlxPci_ChipTypeGet(
-        &Device,
-        &ChipTypeSelected,
-        &ChipRevision
-        );
+    PlxPci_ChipTypeGet(&Device,&ChipTypeSelected,&ChipRevision);
     memset(&PlxInterrupt, 0, sizeof(PLX_INTERRUPT));
     PlxInterrupt.LocalToPci = 1;
-    rc =
-        PlxPci_InterruptEnable(
-            &Device,
-            &PlxInterrupt
-            );
-
-
+    rc =PlxPci_InterruptEnable(&Device,&PlxInterrupt);
 }
+/*
+pciConfig function writes values to registers on the PCI controller to successfully initialize and configure the ADC chip and checks to make sure each configuration write finishes successfully
+*/
 void pciConfig(PLX_DEVICE_OBJECT *Device){
     PLX_STATUS        rc;
-    rc =
-        PlxPci_PciRegisterWriteFast(
-            Device,
-            PLL_address,
-            PLL_register
-            );
+    rc =PlxPci_PciRegisterWriteFast(Device,PLL_address,PLL_register);
 
     if (rc != PLX_STATUS_OK)
     {
-        printf("*ERROR* - \n");
+        printf("*ERROR* First write failed- \n");
 
         return;
     }
-    rc =
-        PlxPci_PciRegisterWriteFast(
-            Device,
-            Rate_divisor_address,
-            Rate_divisor
-            );
+    rc =PlxPci_PciRegisterWriteFast(Device,Rate_divisor_address,Rate_divisor);
 
     if (rc != PLX_STATUS_OK)
     {
-        printf("*ERROR* - API failed\n");
+        printf("*ERROR* - Second write failed\n");
 
         return;
     }
-    rc =
-        PlxPci_PciRegisterWriteFast(
-            Device,
-            BCR_address,
-            BCR_register
-            );
+    rc =PlxPci_PciRegisterWriteFast(Device,BCR_address,BCR_register);
 
     if (rc != PLX_STATUS_OK)
     {
-        printf("*ERROR* - API failed\n");
+        printf("*ERROR* - Third write failed\n");
 
         return;
     }
-    rc =
-        PlxPci_PciRegisterWriteFast(
-            Device,
-            Buffer_address,
-            buffer_on_register
-            );
+    rc =PlxPci_PciRegisterWriteFast(Device,Buffer_address,buffer_on_register);
 
     if (rc != PLX_STATUS_OK)
     {
-        printf("*ERROR* - API failed\n");
+        printf("*ERROR* - Fourth Write failed\n");
 
         return;
     }
     rc=PlxPci_DeviceReset(Device);
     if (rc != PLX_STATUS_OK)
     {
-        printf("Reset\n");
+        printf("Reset failed\n");
 
         return;
     }
 
+}
+/*
+Initialize DMA transfer ability to allow mass data transfer from the ADC via the PCI interface
+*/
+void initDMA(PLX_DEVICE_OBJECT *Device, PLX_DEVICE_KEY *DeviceKey){
+    PLX_STATUS        rc;
+    PLX_DRIVER_PROP   DriverProp;
+    rc =PlxPci_DeviceOpen(&DeviceKey,&Device);
+    printf("*ERROR* - API failed\n");
+    if (rc != PLX_STATUS_OK)
+    {
+        exit(-1);
+    }
+    PlxPci_DriverProperties(&Device,&DriverProp);
+
+    if (DriverProp.bIsServiceDriver)
+        {
+            exit(-1);
+        }
 }
 /*uint32_t pciConfigReadWord (uint8_t bus,
                              uint8_t func, uint8_t offset)
@@ -174,23 +128,12 @@ void pciConfig(PLX_DEVICE_OBJECT *Device){
     return (tmp);
  }
 */
-
-
-
-
-
                 // Open device to get its properties
-
-
             // Verify driver used is PnP and not Service driver
-
-
-
-
-void
-PerformDma_9000(
-    PLX_DEVICE_OBJECT *pDevice, FILE *output
-    )
+/*
+Perform DMA data transfer of data over PCI interface
+*/
+void PerformDma_9000(PLX_DEVICE_OBJECT *pDevice, FILE *output)
 {
     U8                DmaChannel=0;
     U8               *pUserBuffer = NULL;
@@ -203,20 +146,12 @@ PerformDma_9000(
     PLX_PHYSICAL_MEM  PciBuffer;
     PLX_NOTIFY_OBJECT NotifyObject;
 
-     rc =
-        PlxPci_NotificationCancel(
-            pDevice,
-            &NotifyObject
-            );
+     rc =PlxPci_NotificationCancel(pDevice,&NotifyObject);
      memset(&PlxInterrupt, 0, sizeof(PLX_INTERRUPT));
     DmaChannel = (U8)ChannelInput;
 
     // Get DMA buffer parameters
-    PlxPci_CommonBufferProperties(
-        pDevice,
-        &PciBuffer
-        );
-
+    PlxPci_CommonBufferProperties(pDevice,&PciBuffer);
 
     // Clear DMA structure
     memset(&DmaProp, 0, sizeof(PLX_DMA_PROP));
@@ -226,33 +161,18 @@ PerformDma_9000(
     DmaProp.ReadyInput    = 1;
 
 
-    rc =
-        PlxPci_DmaChannelOpen(
-            pDevice,
-            DmaChannel,
-            &DmaProp
-            );
-
-
-
+    rc =PlxPci_DmaChannelOpen(pDevice,DmaChannel,&DmaProp);
     // Clear interrupt fields
     memset(&PlxInterrupt, 0, sizeof(PLX_INTERRUPT));
 
     // Setup to wait for selected DMA channel
     PlxInterrupt.DmaDone = (1 << DmaChannel);
 
-    rc =
-        PlxPci_NotificationRegisterFor(
-            pDevice,
-            &PlxInterrupt,
-            &NotifyObject
-            );
-
+    rc =PlxPci_NotificationRegisterFor(pDevice,&PlxInterrupt,&NotifyObject);
 
     // Transfer the Data
     //Cons_printf("  Perform Block DMA transfer..... ");
-     if (pUserBuffer == NULL)
-        pUserBuffer = malloc(PciBuffer.Size);
+     if (pUserBuffer == NULL) pUserBuffer = malloc(PciBuffer.Size);
     // Clear DMA data
     memset(&DmaParams, 0, sizeof(PLX_DMA_PARAMS));
 
@@ -261,24 +181,14 @@ PerformDma_9000(
     DmaParams.ByteCount = 0x3FF8;
     DmaParams.Direction = PLX_DMA_LOC_TO_PCI;
 
-    rc =
-        PlxPci_DmaTransferBlock(
-            pDevice,
-            DmaChannel,
-            &DmaParams,
+    rc =PlxPci_DmaTransferBlock(pDevice,DmaChannel,&DmaParams,
             0          // Don't wait for completion
             );
 
     if (rc == PLX_STATUS_OK)
     {
-
-
-        rc =
-            PlxPci_NotificationWait(
-                pDevice,
-                &NotifyObject,
-                5 * 1000
-                );
+        //notification to wait for DMA transfer
+        rc =PlxPci_NotificationWait(pDevice,&NotifyObject,5 * 1000);
 
         switch (rc)
         {
@@ -303,19 +213,8 @@ PerformDma_9000(
     else
     {
         printf("*ERROR* - API failed\n");
-
     }
-
-
-    rc =
-        PlxPci_NotificationStatus(
-            pDevice,
-            &NotifyObject,
-            &PlxInterrupt
-            );
-
-
-
+    rc =PlxPci_NotificationStatus(pDevice,&NotifyObject,&PlxInterrupt);
 
     // Allocate a buffer
     pUserBuffer = malloc(0x3FF8);
@@ -328,11 +227,7 @@ PerformDma_9000(
     DmaParams.ByteCount = 0x3FF8;
     DmaParams.Direction = PLX_DMA_LOC_TO_PCI;
 
-    rc =
-        PlxPci_DmaTransferUserBuffer(
-            pDevice,
-            DmaChannel,
-            &DmaParams,
+    rc =PlxPci_DmaTransferUserBuffer(pDevice,DmaChannel,&DmaParams,
             200         // Specify a timeout to let API perform wait
             );
 
@@ -342,73 +237,66 @@ PerformDma_9000(
         }
     long tempdata;
     int i;
+    //loop to iterate for all allocated memory from the DMA transfer operation and writes each entry into an data field in the csv output file.
     for(i=0;i<0x3FF9;i+=4){
         tempdata=(long)((pUserBuffer[i+3]<<24)+(pUserBuffer[i+2]<<16)+(pUserBuffer[i+1]<<8)+pUserBuffer[i]);
         tempdata=tempdata&0x00FFFFFF;
-        fprintf(output,",%l",tempdata);
+        fprintf(output,"/n%d",tempdata);
     }
     free(pUserBuffer);
 
 
 
 }
-
+/*
+Function to close PCI interface, DMA channel and also the data file
+*/
 void stop_sampling(PLX_DEVICE_OBJECT *pDevice,FILE *output){
     PLX_STATUS        rc;
     U8 DmaChannel=0;
-    rc =PlxPci_DeviceClose(
-            &pDevice
-            );
+    rc =PlxPci_DeviceClose(&pDevice);
     // Close DMA Channel
     printf("  Close DMA Channel.............. ");
-    rc =
-        PlxPci_DmaChannelClose(
-            pDevice,
-            DmaChannel
-            );
+    rc =PlxPci_DmaChannelClose(pDevice,DmaChannel);
     fclose(output);
-
     // Release user buffer
-
-
-
 }
-
 
 int main()
 {
     FILE *output;
     char *mode= "w";
-    char *extension=".csv";
+    //char *extension=".csv";
     char *filename;
+    filename="data.csv";
     char exitchar=NULL;
     S16 DeviceSelected;
     PLX_DEVICE_OBJECT Device;
     PLX_DEVICE_KEY    DeviceKey;
     PLX_STATUS        rc;
     PLX_NOTIFY_OBJECT NotifyObject;
-    scanf("%s",filename);
+    //("*ERROR* - API failed\n");
+    //scanf("%s",filename);
+    //printf("*ERROR* - API failed\n");
     Device=pciInit();
+
 
     pciConfig(&Device);
     printf("pci config finished");
 
     initDMA(&Device, &DeviceKey);
-    strcat(filename,extension);
+    //strcat(filename,extension);
     output=fopen(filename,mode);
-    fprintf(output,"Data Start");
-    while(exitchar==NULL){
 
-        rc =
-        PlxPci_NotificationWait(
-            &Device,
-            &NotifyObject,
-            10 * 1000
-            );
+    while(exitchar==NULL){
+        //wait for PCI interface to signal that PCI memory buffer is full before starting DMA transfer
+        rc =PlxPci_NotificationWait(&Device,&NotifyObject,10 * 1000);
         PerformDma_9000(&Device,output);
+        //provide simple exit for program
         exitchar=getch();
 
     }
+    //close PCI interface and DMA data channel
     stop_sampling(&Device,output);
 }
 
